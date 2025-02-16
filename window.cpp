0bd460477a76cf19bc15dcc4ed395d22ab0eda95
RicardoGonVi <ricardogonzalez.11.99@gmail.com>
2025-02-16 01:51:10 -0600

    Add basic color format to main GUI

--- window.cpp
+++ window.cpp
@@ -1,10 +1,7 @@
 #include "window.h"
 #include "./ui_window.h"
 
-Window::Window(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::Window)
-{
+Window::Window(QWidget *parent) : QMainWindow(parent),  ui(new Ui::Window) {
     ui->setupUi(this);
 }
 
